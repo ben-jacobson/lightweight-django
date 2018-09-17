05871cf48f694da742f821b76ee114652b487eeb
Ben Jacobson <ben_jacobson@live.com>
2018-09-18 09:03:08 +1000

    Up to chapter 2, stateless web applications

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,9 +1,18 @@
+import os
 import sys
+
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'			# basically a fancy way of checking if the value is == on, then return true or false. 
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
-	DEBUG=True,
-	SECRET_KEY='thisisthesecretkey',
+	DEBUG=DEBUG,
+	SECRET_KEY=SECRET_KEY,
+	ALLOWED_HOSTS=ALLOWED_HOSTS,
 	ROOT_URLCONF=__name__,
 	MIDDLEWARE_CLASSES=(
 		'django.middleware.common.CommonMiddleware',
@@ -13,6 +22,7 @@ settings.configure(
 )
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 def index(request):
@@ -21,6 +31,8 @@ def index(request):
 urlpatterns = (
 	url(r'^$', index),
 )
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
 	from django.core.management import execute_from_command_line
