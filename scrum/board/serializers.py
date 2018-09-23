79615e8660303385eb83d59b0ba2ee2712c23374
Ben Jacobson <ben_jacobson@live.com>
2018-09-24 08:54:13 +1000

    finished building API app from CH4, up to CH5 now

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 from rest_framework.reverse import reverse
@@ -8,19 +11,29 @@ from .models import Sprint, Task
 User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
-
     links = serializers.SerializerMethodField('get_links')
     
     class Meta:
         model = Sprint
         fields = ('id', 'name', 'description', 'end', 'links', )
-
+        
     def get_links(self, obj):
         request = self.context['request']
         return {
             'self': reverse('sprint-detail',
-            kwargs={'pk': obj.pk},request=request),
-        }        
+                kwargs={'pk': obj.pk}, request=request),
+            'tasks': reverse('task-list',
+                request=request) + '?sprint={}'.format(obj.pk),
+        }
+
+    def validate_end(self, attrs, source):
+        end_date = attrs[source]
+        new = not self.object
+        changed = self.object and self.object.end != end_date
+        if (new or changed) and (end_date < date.today()):
+            msg = _('End date cannot be in the past.')
+            raise serializers.ValidationError(msg)
+        return attrs
 
 class TaskSerializer(serializers.ModelSerializer):
     assigned = serializers.SlugRelatedField(
@@ -51,9 +64,42 @@ class TaskSerializer(serializers.ModelSerializer):
         if obj.assigned:
             links['assigned'] = reverse('user-detail',
                 kwargs={User.USERNAME_FIELD: obj.assigned}, request=request)
-        return links      
+        return links   
 
-class UserSerializer(serializers.ModelSerializer):
+    def validate_sprint(self, attrs, source):
+        sprint = attrs[source]
+        if self.object and self.object.pk:
+            if sprint != self.object.sprint:
+                if self.object.status == Task.STATUS_DONE:
+                    msg = _('Cannot change the sprint of a completed task.')
+                    raise serializers.ValidationError(msg)
+                if sprint and sprint.end < date.today():
+                    msg = _('Cannot assign tasks to past sprints.')
+                    raise serializers.ValidationError(msg)
+            else:
+                if sprint and sprint.end < date.today():
+                    msg = _('Cannot add tasks to past sprints.')
+                    raise serializers.ValidationError(msg)
+            return attrs
+
+    def validate(self, attrs):
+        sprint = attrs.get('sprint')
+        status = int(attrs.get('status'))
+        started = attrs.get('started')
+        completed = attrs.get('completed')
+        if not sprint and status != Task.STATUS_TODO:
+            msg = _('Backlog tasks must have "Not Started" status.')
+            raise serializers.ValidationError(msg)
+        if started and status == Task.STATUS_TODO:
+            msg = _('Started date cannot be set for not started tasks.')
+            raise serializers.ValidationError(msg)
+        if completed and status != Task.STATUS_DONE:
+            msg = _('Completed date cannot be set for uncompleted tasks.')
+            raise serializers.ValidationError(msg)
+        return attrs    
+
+
+class UserSerializer(serializers.ModelSerializer):    
     full_name = serializers.CharField(source='get_full_name', read_only=True)
     links = serializers.SerializerMethodField('get_links')  
 
@@ -67,5 +113,7 @@ class UserSerializer(serializers.ModelSerializer):
         username = obj.get_username()
         return {
             'self': reverse('user-detail',
-            kwargs={User.USERNAME_FIELD: username}, request=request),
-        }            
+                kwargs={User.USERNAME_FIELD: username}, request=request),
+            'tasks': '{}?assigned={}'.format(
+                reverse('task-list', request=request), username)
+        }           
--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+from .forms import TaskFilter, SprintFilter
 
 User = get_user_model()
 
@@ -11,23 +12,33 @@ class DefaultsMixin(object):
         authentication.BasicAuthentication,
         authentication.TokenAuthentication,
     )
-    
     permission_classes = (
         permissions.IsAuthenticated,
     )
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
-
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+  
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
-    '''API endpoint for listing and creating sprints'''
+    """API endpoint for listing and creating sprints."""
     queryset = Sprint.objects.order_by('end')
-    serializer_class = SprintSerializer 
+    serializer_class = SprintSerializer
+    filter_class = SprintFilter
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
     '''API endpoint for listing and creating tasks.'''
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_class = TaskFilter
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     '''API endpoint for listing users.'''
@@ -35,3 +46,4 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer    
+    search_fields = (User.USERNAME_FIELD, )    
